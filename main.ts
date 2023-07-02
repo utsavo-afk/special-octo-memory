69a9c1f311c57eb53e9fe66c8be0376e1ac44b7e
utsav ojha <utsavojha@utsavs-MacBook-Air.local>
2023-07-02 18:42:28 +0530

    find queries

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import { connectToDb } from "./db/helpers.ts";
 import { uri } from "./constants/index.ts";
-import { create, findAll, findOneByUsernameOrEmail } from "./users/service.ts";
+import {
+  create,
+  findAll,
+  findById,
+  findOneByUsernameOrEmail,
+} from "./users/service.ts";
 
 /**
  * {
@@ -11,14 +16,17 @@ import { create, findAll, findOneByUsernameOrEmail } from "./users/service.ts";
     createdAt: ISODate("2023-07-02T12:50:46.507Z"),
     updatedAt: ISODate("2023-07-02T12:50:46.507Z")
   },
- */
+*/
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   await connectToDb({ uri });
 
   try {
+    // await create({ email: "xyz@abc.com", username: "xyz abc" });
     // await findAll();
+    // await findById("64a172a6786d049497fb9d8d");
+    // await findOneByUsernameOrEmail("Lura Brock");
+    // await findOneByUsernameOrEmail("ga@tivi.in");
   } catch (error) {
     console.error(error.message);
   }
